cd49ad7e393b5503bb2129e5fec5d29a76ea59dd
agamairi <agam.airi@gmail.com>
2024-12-29 23:25:45 -0500

    refactoring

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const sectButtons = document.querySelectorAll('.controls');
 const Button = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
-function TransitionPage(){
+function TransitionPage() {
     // Activates the button active class
-    for(let i = 0; i < Button.length; i++){
-        Button[i].addEventListener('click', function() {
+    for (let i = 0; i < Button.length; i++) {
+        Button[i].addEventListener('click', function () {
             let selectedButton = document.querySelectorAll('.activebtn');
-            selectedButton[0].className = selectedButton[0].className.replace('activebtn','');
+            selectedButton[0].className = selectedButton[0].className.replace('activebtn', '');
             this.className += ' activebtn';
         })
     }
-// Putting the section on top of each other. Kind of like stacking
+    // Putting the section on top of each other. Kind of like stacking
     allSections.addEventListener('click', (e) => {
         const id = e.target.dataset.id;
-        if(id){
+        if (id) {
             // removes the current page
-            sectButtons.forEach((btn)=>{
+            sectButtons.forEach((btn) => {
                 btn.classList.remove('active')
 
             })
             e.target.classList.add('active')
-            sections.forEach((section)=>{
+            sections.forEach((section) => {
                 section.classList.remove('active')
             })
 
